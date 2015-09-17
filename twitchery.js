71591ccd0b3a8f3aac184dcce71a61b6fb88ff1b
Harvey Ngo <harveydngo@gmail.com>
2015-09-17 00:01:26 -0700

    Setup base HTML and JavaScript file.
    Setup the index.html file to include a form to represent the search bar and a section where the results will by dynamically rendered. Add event listener to the form, where upon submission, it will hit the target Twitch API route. Throw an alert to notify the user that the route was hit and console.log out the XHR response text.

---
+++ twitchery.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function(){
+	console.log("game starto")
+
+	var searchForm = document.getElementsByTagName("form")[0]
+	searchForm.addEventListener("submit", returnSearchResults)
+
+	//lesson learned: without the event.preventDefault(), the search bar would automatically clear its content and remove the dynamically appended content after being submitted
+	function returnSearchResults(event){
+		event.preventDefault()
+
+		var xmlhttp;
+		if (window.XMLHttpRequest)
+		  {// code for IE7+, Firefox, Chrome, Opera, Safari
+		  xmlhttp=new XMLHttpRequest();
+		  }
+		else
+		  {// code for IE6, IE5
+		  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		xmlhttp.onreadystatechange=function()
+		  {
+		  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		    {
+		    alert('hit the api')
+		    console.log(xmlhttp.responseText)
+		    }
+		  }  
+		xmlhttp.open("GET", "https://api.twitch.tv/kraken/search/streams?q=starcraft", true)
+		xmlhttp.send()
+		// var searchResults = document.getElementById('search-results')
+		// var searchBar = this.children[0]
+		// alert(searchBar.value)
+		// var resultDiv = document.createElement('div')
+		// searchResults.appendChild(resultDiv)
+	}
+
+
+})
+
